32261a4f7ba778faa6ffaae5125ff23cae7698fa
Dohyeon Ju <48502765+dohyeon2@users.noreply.github.com>
2026-09-18 01:46:43 +0900

    test: isolate project promo banner styles

--- scripts/test-project-promo-banner.py
+++ scripts/test-project-promo-banner.py
@@ -12,9 +12,10 @@ def test_project_promo_banner_contract():
     default = read("default.hbs")
     partial = read("partials/project-promo-banner.hbs")
     script = read("assets/js/project-promo-banner.js")
-    styles = read("assets/css/screen.css")
+    styles = read("assets/css/project-promo-banner.css")
 
     assert '{{> "project-promo-banner"}}' in default
+    assert '<link rel="stylesheet" href="{{asset "css/project-promo-banner.css"}}">' in default
     assert 'src="{{asset "js/project-promo-banner.js"}}"' in default
 
     assert 'data-project-promo' in partial
@@ -22,13 +23,17 @@ def test_project_promo_banner_contract():
     assert 'https://dohyeon-kr.github.io/free-rider/' in partial
     assert 'aria-live="off"' in partial
     assert 'data-project-promo-slide' in partial
+    assert 'data-project-promo-toggle' in partial
 
     assert "prefers-reduced-motion: reduce" in script
     assert "pointerenter" in script
     assert "focusin" in script
+    assert "visibilitychange" in script
     assert "5000" in script
 
     assert ".project-promo" in styles
+    assert ".project-promo__slide--poli" in styles
+    assert ".project-promo__slide--free-rider" in styles
     assert "@media (max-width:" in styles
 
 
